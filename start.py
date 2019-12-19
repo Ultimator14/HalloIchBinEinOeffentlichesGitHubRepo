93f506ab5048edb4a1865f3f7d238ed957110163
Jan Breig <jan.breig@freenet.de>
2019-12-19 15:55:31 +0100

    Fix hash error

--- start.py
+++ start.py
@@ -1,19 +1,16 @@
 from User import User
 from Password import Password
-import hashlib
 
-password = "123_x&5s"
-hash_object = hashlib.md5(b'123_x32&')
+p = Password()
+password = "123_x&5s".encode('utf-8')
+hashed_password = p.hash_password(password)
 
 user1 = User()
 user1.set_name("Bert")
-
-p = Password()
-hashed_password = p.hash_password(password)
 user1.set_password(hashed_password)
 
-hashed_password = user1.get_password()
-if p.hash_check(password, hashed_password):
+hashed_password2 = user1.get_password()
+if p.hash_check(password, hashed_password2):
     print("Yes")
 else:
     print("No")
